b33fa9a20aff3c2228a012400b15439f547614d9
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-01-18 09:07:01 -0300

    Name input validation - Questions activity

--- app/src/main/java/com/danieljrodrigues/flagsquizapp/MainActivity.kt
+++ app/src/main/java/com/danieljrodrigues/flagsquizapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.danieljrodrigues.flagsquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +13,18 @@ class MainActivity : AppCompatActivity() {
         this.supportActionBar?.hide();
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var submitBtn = findViewById<Button>(R.id.submitBtn)
+        var nameInput = findViewById<AppCompatEditText>(R.id.nameInput)
+
+        submitBtn.setOnClickListener {
+            if (nameInput.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please, enter your name", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
