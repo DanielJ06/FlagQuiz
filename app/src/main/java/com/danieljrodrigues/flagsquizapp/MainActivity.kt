4b6ad605bf32d7eaf66bae510be5565c8aaddf9d
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-01-19 15:37:55 -0300

    KAE instead findViewById

--- app/src/main/java/com/danieljrodrigues/flagsquizapp/MainActivity.kt
+++ app/src/main/java/com/danieljrodrigues/flagsquizapp/MainActivity.kt
@@ -5,21 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.AppCompatEditText
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         this.supportActionBar?.hide();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var submitBtn = findViewById<Button>(R.id.submitBtn)
-        var nameInput = findViewById<AppCompatEditText>(R.id.nameInput)
-
         submitBtn.setOnClickListener {
             if (nameInput.text.toString().isEmpty()) {
-                Toast.makeText(this, "Please, enter your name", Toast.LENGTH_LONG).show()
+                Snackbar.make(it, "Please, enter your name", 2000).setBackgroundTint(resources.getColor(R.color.purple_500)).show()
             } else {
                 val intent = Intent(this, QuizQuestionActivity::class.java)
                 startActivity(intent)
--- app/src/main/java/com/danieljrodrigues/flagsquizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/danieljrodrigues/flagsquizapp/QuizQuestionActivity.kt
@@ -2,10 +2,28 @@ package com.danieljrodrigues.flagsquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_quiz_question.*
 
 class QuizQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        this.supportActionBar?.hide();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+
+        val questionList = Constants.getQuestions()
+        val currentPosition = 1
+        val question: Question? = questionList[currentPosition - 1]
+
+        questionLabel.text = question!!.question
+        flagImg.setImageResource(question.image)
+        progressBar.progress = currentPosition
+        textProgress.text = "$currentPosition/${progressBar.max}"
+
+        optionOne.text = question.optOne
+        optionTwo.text = question.optTwo
+        optionThree.text = question.optThree
+        optionFour.text = question.optFour
     }
 }
